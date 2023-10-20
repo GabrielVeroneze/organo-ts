86ef2bcdb536b505ffd5d7aebe4e9cb912ef8594
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-10-20 19:58:50 -0300

    :sparkles: feat: Migra o TIme para TypeScript

---
+++ src/componentes/Time/index.tsx
@@ -0,0 +1,40 @@
+import { IColaborador } from '../../compartilhado/interfaces/IColaborador'
+import Colaborador from '../Colaborador'
+import './Time.css'
+
+interface TimeProps {
+    nome: string
+    corPrimaria: string
+    corSecundaria: string
+    colaboradores: IColaborador[]
+}
+
+const Time = ({ nome, corPrimaria, corSecundaria, colaboradores } : TimeProps) => {    
+    return ( 
+        colaboradores.length > 0 && (
+            <section
+                className='time'
+                style={{ backgroundColor: corSecundaria }}
+            >
+                <h3 className='time__titulo'>{nome}</h3>
+                <div 
+                    className='time__sublinhado' 
+                    style={{backgroundColor: corPrimaria}}
+                ></div>
+                <div className='time__colaboradores'>
+                    {colaboradores.map(colaborador => (
+                        <Colaborador
+                            key={colaborador.nome}
+                            nome={colaborador.nome}
+                            cargo={colaborador.cargo}
+                            imagem={colaborador.imagem}
+                            corDeFundo={corPrimaria}
+                        />
+                    ))}
+                </div>
+            </section>
+        ) 
+    )
+}
+
+export default Time
