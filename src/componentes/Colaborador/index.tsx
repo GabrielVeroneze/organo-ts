9eccad112b12f2765b045a7c07e9699699087049
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-10-20 19:44:46 -0300

    :sparkles: feat: Migra o Colaborador para TypeScript

--- src/componentes/Colaborador/index.tsx
+++ src/componentes/Colaborador/index.tsx
@@ -1,10 +1,19 @@
 import './Colaborador.css'
 
-const Colaborador = ({nome, cargo, imagem, corDeFundo}) => {
+interface ColaboradorProps {
+    nome: string
+    cargo: string
+    imagem: string
+    corDeFundo: string
+}
 
+const Colaborador = ({ nome, cargo, imagem, corDeFundo } : ColaboradorProps) => {
     return (
         <div className='colaborador'>
-            <div className='colaborador__cabecalho' style={{backgroundColor: corDeFundo}}>
+            <div
+                className='colaborador__cabecalho'
+                style={{ backgroundColor: corDeFundo }}
+            >
                 <img className='colaborador__imagem' src={imagem} alt={nome} />
             </div>
             <div className='colaborador__rodape'>
